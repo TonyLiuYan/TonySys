d8e7a6a8ce692fc9dec25764b7986abbc7acbbe5
yan.liu <yan.liu@sumscope.com>
2016-09-06 13:36:01 +0800

    1

--- TonySys/TonySys/Areas/WeiXinArea/Controllers/HomeController.cs
+++ TonySys/TonySys/Areas/WeiXinArea/Controllers/HomeController.cs
@@ -10,6 +10,8 @@ namespace TonySys.Areas.WeiXinArea.Controllers
 {
     public class HomeController : Controller
     {
+        readonly log4net.ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         /// <summary>
         /// 定义Token，与微信公共平台上的Token保持一致
         /// </summary>
@@ -23,13 +25,19 @@ namespace TonySys.Areas.WeiXinArea.Controllers
         private bool CheckSignature(WeChatRequestModel model)
         {
             //创建数组，将 Token, timestamp, nonce 三个参数加入数组
+            logger.Debug("Token=" + Token);
+            logger.Debug("Timestamp=" + model.Timestamp);
+            logger.Debug("Nonce=" + model.Nonce);
             string[] array = { Token, model.Timestamp, model.Nonce };
             //进行排序
             Array.Sort(array);
             //拼接为一个字符串
             var tempStr = String.Join("", array);
+            logger.Debug("tempStr=" + tempStr);
             //对字符串进行 SHA1加密
             tempStr = FormsAuthentication.HashPasswordForStoringInConfigFile(tempStr, "SHA1").ToLower();
+            logger.Debug("SHA1_tempStr=" + tempStr);
+            logger.Debug("Signature=" + model.Signature);
             //判断signature 是否正确
             if (tempStr.Equals(model.Signature))
             {
@@ -44,19 +52,24 @@ namespace TonySys.Areas.WeiXinArea.Controllers
 
         public void Valid(WeChatRequestModel model)
         {
+            logger.Debug("开始验证");
             //获取请求来的 echostr 参数
             string echoStr = model.Echostr;
+            logger.Debug("echoStr=" + echoStr);
             //通过验证
             if (CheckSignature(model))
             {
                 if (!string.IsNullOrEmpty(echoStr))
                 {
                     //将随机生成的 echostr 参数 原样输出
+                    logger.Debug("输出echoStr=" + echoStr);
                     Response.Write(echoStr);
                     //截止输出流
                     Response.End();
                 }
             }
+
+            logger.Logger.Repository.Shutdown();
         }
     }
 }
